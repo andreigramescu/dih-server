604db9c8a64a9e5a079abfa2aa3985ba92c4eca1
Andrei Gramescu <andrei.gramescu@gmail.com>
2023-06-17 09:50:12 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, make_response, render_template, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+dih_cache = {} # query -> response@(headers, body)
+is_fresh = True
+
+def fmt(resolutions):
+    SEP = '|'
+    return ','.join([f"{h}{SEP}{ip}{SEP}{ttl}" for (h, ip, ttl) in resolutions])
+
+def parse_doh_json(body):
+    if 'Answer' not in body:
+        return None
+
+    for answer in body['Answer']:
+        if answer['type'] == 1: # record of type A
+            return answer['name'], answer['data'], int(answer['TTL'])
+
+    return None
+
+def google_doh(domain):
+    try:
+        url = "https://dns.google/resolve"
+        params = { "name": domain, "type": "A" }
+        response = requests.get(url, params=params)
+    except:
+        return None
+    if response.status_code != 200:
+        return None
+    body = response.json()
+    return parse_doh_json(body)
+
+def parse_host(question):
+    curr_count = question[0]
+    res = []
+    while curr_count != 0:
+        question = question[1:]
+        res.append("")
+        for _ in range(curr_count):
+            res[-1] += chr(question[0])
+            question = question[1:]
+        curr_count = question[0]
+    return '.'.join(res)
+
+@app.route('/fresh-true')
+def fresh_true():
+    is_fresh = True
+    dih_cache = {}
+    return str(is_fresh), 200, {}
+
+@app.route('/fresh-false')
+def fresh_false():
+    is_fresh = False
+    return str(is_fresh), 200, {}
+
+@app.route('/')
+def index():
+    body = render_template('index.html')
+
+    if is_fresh:
+        headers = {}
+    else:
+        hostnames = ["www.example.com", "www.andreigramescu.com"]
+        resolutions = [google_doh(hostname) for hostname in hostnames]
+        resolutions = filter(lambda x: x is not None, resolutions)
+        resolutions = map(lambda x: (x[0], x[1], str(x[2])), resolutions)
+        resolutions = list(resolutions)
+
+        headers = {'X-Dih': f'{fmt(resolutions)}'}
+
+    return body, 200, headers
+
+@app.route('/cache', methods=['POST'])
+def cache():
+    r = dict((parse_host(k), str(v[1])) for (k, v) in dih_cache.items())
+    print(r)
+    return jsonify(r)
+
+@app.route('/dns-query', methods=['GET', 'POST'])
+def handle_doh():
+    print(f"Request for {parse_host(aux_data[12:])}")
+
+    aux_hdrs = dict(request.headers.to_wsgi_list())
+    aux_data = request.data
+    if is_fresh:
+        URL = "https://dns.google/dns-query"
+        aux = requests.post(URL, headers=aux_hdrs, data=aux_data)
+
+        dih_cache[aux_data] = (dict(aux.headers), aux.content)
+    else:
+        hdrs, raw_data = dih_cache[aux_data]
+
+    return raw_data, 200, hdrs
+
+if __name__ == '__main__':
+    app.run()
